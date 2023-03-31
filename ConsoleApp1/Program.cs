5e07a9fb51e72f254f4bacc4c42be2dcbbda56ea
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-31 12:32:21 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int number = 0;
+        Console.Write("Enter a number : ");
+        int inputNumber = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[inputNumber];
+        int oddNumber;
+        while (inputNumber != 0)
+        {
+            oddNumber = inputNumber % 10;
+            inputNumber = inputNumber / 10;
+            if (oddNumber % 2 != 0)
+            {
+                number = oddNumber;
+                
+            }
+        }
+            Console.WriteLine($"New massive which possess a length of first odd number is {number} : ");
+            for (int i = 1; i <= number; i++)
+            {
+                  arr[i] = random.Next(1,10);
+            Console.WriteLine(arr[i]);
+
+        }
+         }
+}
